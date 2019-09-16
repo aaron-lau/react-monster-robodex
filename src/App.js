74c2d024f4b2e09995297d4a4875493949116dc8
Aaron Lau <aaron.lau@datadoghq.com>
2019-09-15 23:01:13 -0400

    using map to print monnster names

--- src/App.js
+++ src/App.js
@@ -7,27 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Aaron Lau'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Zombie',
+          id: '2'
+        },
+        {
+          name: 'Vampire',
+          id: '3'
+        },
+      ]
     }
   }
+
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => this.setState({string: 'Hello not Aaron'})}> </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name } </h1>
+        ))}
       </div>
       )
   }
