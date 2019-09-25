b2368044e988baf961d9fe09aac22afecf09326c
Aaron Lau <aaron.lau@datadoghq.com>
2019-09-25 16:52:44 -0400

    Updates

---
+++ precache-manifest.d59a82de0f4392084528fda30862b038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d8144af7136f6d8a721226479a5085",
+    "url": "/react-monsters-robodex/index.html"
+  },
+  {
+    "revision": "839acc0d8c6c38cf2547",
+    "url": "/react-monsters-robodex/static/css/main.d761467a.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/react-monsters-robodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "839acc0d8c6c38cf2547",
+    "url": "/react-monsters-robodex/static/js/main.ae1034c1.chunk.js"
+  },
+  {
+    "revision": "0e891708678e07d16a2a",
+    "url": "/react-monsters-robodex/static/js/runtime~main.2b145c2e.js"
+  }
+]);
